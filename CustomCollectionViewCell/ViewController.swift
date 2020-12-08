98644d63af080638c134fb572a4c725db9fb505c
Duncan Champney <DChampney@wareto.com>
2020-12-07 19:43:05 -0500

    Initial Commit

---
+++ CustomCollectionViewCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCollectionViewCell
+//
+//  Created by Duncan Champney on 12/7/20.
+//  Copyright © 2020 Duncan Champney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
